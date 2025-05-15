431340d5d724f782c8125ce14a1044f533aaf47c
intel <93615583+intel1337@users.noreply.github.com>
2025-05-15 09:19:36 +0200

    dev branch

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import Link from "next/link";
+import styles from "./page.module.css"
+
+
+export default function LandingPage(){
+  return(
+    <div className={styles.LandingPage}>
+
+    </div>
+  )
+}
